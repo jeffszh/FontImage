1d57337fb22b422ecb22b9d8c664fd6fa23c6210
Jeff <zhsu@amwares.com>
2020-06-30 17:00:27 +0800

    非也，本身就已经是透明的了，虽然用另一种方法也行，毕竟awt是旧方法，还是不采用。

--- src/main/kotlin/cn/jeff/tools/MainWndK.kt
+++ src/main/kotlin/cn/jeff/tools/MainWndK.kt
@@ -12,6 +12,8 @@ class MainWndK(private val j: MainWnd) {
 
 	fun btnStartClick() {
 		val gc = j.workCanvas.graphicsContext2D
+		gc.fill = Color.TRANSPARENT
+		gc.fillRect(30.0, 20.0, 100.0, 50.0)
 		gc.font = Font.font("Courier New", 24.0)
 		gc.stroke = Paint.valueOf("BLUE")
 		gc.strokeText("ABC abc", 30.0, 20.0)
@@ -21,6 +23,18 @@ class MainWndK(private val j: MainWnd) {
 		}
 		val img = j.workCanvas.snapshot(snapshotParameters, null)
 		ImageIO.write(SwingFXUtils.fromFXImage(img, null), "png", File("myPic.png"))
+
+//		val bufferedImage = BufferedImage(100, 50, BufferedImage.TYPE_INT_ARGB)
+//		var gra = bufferedImage.createGraphics()
+//		val image = gra.deviceConfiguration.createCompatibleImage(100, 50, Transparency.TRANSLUCENT)
+//		gra.dispose()
+//		gra = image.createGraphics()
+//		gra.font = java.awt.Font("Courier New", java.awt.Font.PLAIN, 24)
+//		gra.color = java.awt.Color.RED
+//		gra.drawString("ABC 123 abc", 30, 20)
+//		gra.drawLine(10, 20, 30, 40)
+//		gra.dispose()
+//		ImageIO.write(image, "png", File("pic2.png"))
 	}
 
 }
