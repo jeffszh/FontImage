6195e678572eeab813e1f979ce741b6263690716
Jeff <zhsu@amwares.com>
2020-07-01 12:33:35 +0800

    有点靠谱了。

--- src/main/kotlin/cn/jeff/tools/MainWnd.java
+++ src/main/kotlin/cn/jeff/tools/MainWnd.java
@@ -2,12 +2,14 @@ package cn.jeff.tools;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 
 public class MainWnd {
 
 	private MainWndK k = new MainWndK(this);
 	public Button btnStart;
 	public Canvas workCanvas;
+	public FlowPane outputPane;
 
 	public void btnStartClick() {
 		k.btnStartClick();
--- src/main/kotlin/cn/jeff/tools/MainWndK.kt
+++ src/main/kotlin/cn/jeff/tools/MainWndK.kt
@@ -1,40 +1,67 @@
 package cn.jeff.tools
 
 import javafx.embed.swing.SwingFXUtils
+import javafx.geometry.Rectangle2D
 import javafx.scene.SnapshotParameters
+import javafx.scene.canvas.Canvas
 import javafx.scene.paint.Color
 import javafx.scene.paint.Paint
 import javafx.scene.text.Font
-import java.io.File
-import javax.imageio.ImageIO
+import javafx.scene.text.Text
+import kotlin.math.ceil
 
 class MainWndK(private val j: MainWnd) {
 
-	fun btnStartClick() {
-		val gc = j.workCanvas.graphicsContext2D
-		gc.fill = Color.TRANSPARENT
-		gc.fillRect(30.0, 20.0, 100.0, 50.0)
-		gc.font = Font.font("Courier New", 24.0)
-		gc.stroke = Paint.valueOf("BLUE")
-		gc.strokeText("ABC abc", 30.0, 20.0)
-//		gc.fillText("ABC abc", 30.0, 20.0)
-		val snapshotParameters = SnapshotParameters().also {
-			it.fill = Color.TRANSPARENT
-		}
-		val img = j.workCanvas.snapshot(snapshotParameters, null)
-		ImageIO.write(SwingFXUtils.fromFXImage(img, null), "png", File("myPic.png"))
+	companion object {
+		const val outputPath = "font-png/"
+	}
 
-//		val bufferedImage = BufferedImage(100, 50, BufferedImage.TYPE_INT_ARGB)
-//		var gra = bufferedImage.createGraphics()
-//		val image = gra.deviceConfiguration.createCompatibleImage(100, 50, Transparency.TRANSLUCENT)
-//		gra.dispose()
-//		gra = image.createGraphics()
-//		gra.font = java.awt.Font("Courier New", java.awt.Font.PLAIN, 24)
-//		gra.color = java.awt.Color.RED
-//		gra.drawString("ABC 123 abc", 30, 20)
-//		gra.drawLine(10, 20, 30, 40)
-//		gra.dispose()
-//		ImageIO.write(image, "png", File("pic2.png"))
+	fun btnStartClick() {
+		// 计算字体尺寸
+		val tempText = Text("A")
+		tempText.font = Font.font("Courier New", 26.0)
+		val (xSize, ySize) = with(tempText.layoutBounds) {
+			ceil(width + 1) to ceil(height)
+		}
+		println("$xSize, $ySize")
+
+		val desiredFontColors = arrayOf("black", "red", "blue")
+		desiredFontColors.forEach { fontColor ->
+			val gc = j.workCanvas.graphicsContext2D
+			gc.fill = Color.TRANSPARENT
+			gc.fillRect(0.0, 0.0, 800.0, 200.0)
+			gc.font = tempText.font
+			gc.stroke = Paint.valueOf(fontColor)
+//		gc.strokeText("ABC abc", 30.0, 20.0)
+////		gc.fillText("ABC abc", 30.0, 20.0)
+//		val snapshotParameters = SnapshotParameters().also {
+//			it.fill = Color.TRANSPARENT
+//			it.viewport = Rectangle2D(30.0, 10.0, 50.0, 40.0)
+//		}
+//		val img = j.workCanvas.snapshot(snapshotParameters, null)
+//		ImageIO.write(SwingFXUtils.fromFXImage(img, null), "png", File("myPic.png"))
+
+			val upperCaseLetters = ('A'..'Z').toList().toCharArray()
+			val lowerCaseLetters = ('a'..'z').toList().toCharArray()
+			val digitNumbers = ('0'..'9').toList().toCharArray()
+			val characterArrays = arrayOf(upperCaseLetters, lowerCaseLetters, digitNumbers)
+
+			characterArrays.forEachIndexed { y, chars ->
+				chars.forEachIndexed { x, c ->
+					gc.strokeText(c.toString(), x * xSize + 1, y * ySize + ySize)
+				}
+				chars.forEachIndexed { i, _ ->
+					val snapshotParameters = SnapshotParameters().also {
+						it.fill = Color.TRANSPARENT
+						it.viewport = Rectangle2D(i * xSize, y * ySize + 6, xSize, ySize)
+					}
+					val img = j.workCanvas.snapshot(snapshotParameters, null)
+					j.outputPane.children.add(Canvas(xSize, ySize).also {
+						it.graphicsContext2D.drawImage(img, 0.0, 0.0)
+					})
+				}
+			}
+		}
 	}
 
 }
